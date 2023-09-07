682630b2eb1420bb0fe458fdbdc201f56e897a2a
Vladislav Shilov <vlad.shilov03@mail.ru>
2023-09-08 01:34:18 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+</script>
+
+<template>
+  <header>
+      <HelloWorld />
+  </header>
+
+  <main>
+    <TheWelcome />
+  </main>
+</template>
+
+<style scoped>
+header{
+  margin-left: 10px;
+  
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,40 @@
+<template>
+  <img src="./icons/Group 11.png" @click="changeTheme"><img>
+</template>
+
+<script>
+export default {
+  
+  methods: {
+      changeTheme() {
+  if (document.body.classList.contains('light-theme')) {
+    document.body.classList.remove('light-theme');
+    document.body.classList.add('dark-theme');
+  } else {
+    document.body.classList.remove('dark-theme');
+    document.body.classList.add('light-theme');
+  }
+}
+}
+}
+</script>
+
+<style>
+
+:root {
+--color-background-light: #ffffff;
+--color-background-dark: #000000;
+--color-text-light: #000000;
+--color-text-dark: #ffffff;
+}
+
+body.light-theme {
+background-color: var(--color-background-light);
+color: var(--color-text-light);
+}
+
+body.dark-theme {
+background-color: var(--color-background-dark);
+color: var(--color-text-dark);
+}
+</style>
